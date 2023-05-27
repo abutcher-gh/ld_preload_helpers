374d89ce39b57b1292fd6080a4383bae2b3f461d
abutcher-gh <abutcher-gh@users.noreply.github.com>
2023-05-27 12:53:13 +0100

    Initial commit.

---
+++ README.md
@@ -0,0 +1,76 @@
+# ld_preload_helpers
+
+Rust macros to run code at load time and override C functions.
+
+Mostly useful for `LD_PRELOAD` hooks, hence the name.
+
+# Examples
+
+## C function overrides
+
+Here `open` is overridden to print something then forward to the real `open`.
+In the rare case that you should wish to recurse into the override, rather than
+call the real API, use `crate::open`.  From anywhere else in the module, `open`
+will refer to the override.
+
+Multiple overrides may be specified within a single macro invocation and
+multiple `extern_c_overrides` invocations are supported if desired.  This is
+demonstrated below by an override of `getuid` which adds 2000 to the user's id.
+
+In `std` mode (the default), panics in the override are caught and cause the catch
+block to execute.  In `no_std` mode (not properly supported due to use of
+`OnceLock`; see below), there is no special `panic` handling and the catch block is
+ignored.
+
+```rust
+extern_c_overrides! {
+  unsafe fn open(pathname: *const c_char, flags: c_int, mode: libc::mode_t) -> c_int {
+    println!("RUST OPEN {:?} {:x} {:x}", unsafe { CStr::from_ptr(pathname) }, flags, mode);
+    // panic!("oops");
+    return open(pathname, flags, mode);
+  } catch {
+    errno::set_errno(errno::Errno(libc::ENODEV));
+    return -1;
+  }
+
+  unsafe fn getuid() -> libc::uid_t {
+    getuid() + 2000
+  } catch { u32::MAX }
+}
+```
+
+## Image load hook
+
+Called whenever the program/library is loaded, prior to the main entry point
+being run.  Note this demonstrates calling the overridden `getuid`.
+
+Currently there can be only one of these owing to a fixed name being used for
+the generated function.  But this may be extended in future to allow the user
+to pass a name.
+
+```rust
+on_load! {{
+  println!("INIT LIB {}", unsafe { getuid() });
+}}
+```
+
+## Example runs
+
+```sh
+$ LD_PRELOAD=target/debug/liboverride.so id
+INIT LIB 2501
+uid=2501 gid=501 euid=501
+```
+
+```sh
+$ LD_PRELOAD=target/debug/liboverride.so wc Cargo.lock
+INIT LIB 2501
+RUST OPEN "Cargo.lock" 0 52c47940
+  820  1588 21572 Cargo.lock
+```
+
+# Notes on `no_std` usage
+
+- `std::sync::OnceLock` is the only dependency on `std`.  Should consider `conquer_once` or similar crates to support `no_std`.
+- Could also disable the need for this in single thread code to allow `no_std`.
+- TODO: In `no_std` client code, the catch block is unused; consider not requiring/accepting it in that case.
---
+++ src/lib.rs
@@ -0,0 +1,64 @@
+// OnceLock is the only dependency on std; consider conquer_once or similar crates to support no_std.
+// It has to be exported since the macros using it expand in user code.
+// Could also disable the need for this in single thread code to allow no_std.
+// TODO: In no_std client code, the catch block is unused; consider conditionally accepting it.
+pub use std::sync::OnceLock;
+
+#[macro_export]
+macro_rules! on_load {
+    ($ctor_body:block) => {
+        #[no_mangle]
+        #[link_section = ".init_array"]
+        pub static ld_preload_init: extern "C" fn() = self::ld_preload_on_load;
+        extern "C" fn ld_preload_on_load() { $ctor_body }
+    };
+}
+
+#[macro_export]
+macro_rules! extern_c_overrides {
+    (unsafe fn $c_api:ident($($param_name:ident : $param_type:ty),*) -> $return_type:ty $override_body:block catch $catch_body:block $($more_tokens:tt)*) => {
+        #[no_mangle]
+        pub unsafe extern "C" fn $c_api($($param_name: $param_type),*) -> $return_type {
+            #[link(name = "dl")]
+            extern "C" {
+                #[allow(dead_code)]
+                pub fn dlsym(handle: *const libc::c_void, symbol: *const libc::c_char) -> *const libc::c_void;
+            }
+
+            #[allow(non_camel_case_types)]
+            type $c_api = fn ($($param_name: $param_type),*) -> $return_type;
+
+            #[allow(non_upper_case_globals)]
+            static _dl_resolver: $crate::OnceLock<$c_api> = $crate::OnceLock::new();
+
+            #[allow(unused)]
+            let $c_api = _dl_resolver.get_or_init(|| {
+                let sym = dlsym(-1isize as *const libc::c_void, concat!(stringify!($c_api), "\0").as_ptr() as *const libc::c_char);
+                if sym.is_null() {
+                    panic!("dlsym: Cannot get address for {}", stringify!($c_api));
+                }
+                return core::mem::transmute(sym);
+            });
+
+            extern_c_overrides_body!($override_body, $catch_body)
+        }
+
+        extern_c_overrides! { $($more_tokens)* }
+    };
+    () => {};
+}
+
+#[cfg(not(feature = "std"))]
+#[macro_export]
+macro_rules! extern_c_overrides_body {
+    ($override_body:block, $catch_body:block) => { $override_body };
+}
+
+#[cfg(feature = "std")]
+#[macro_export]
+macro_rules! extern_c_overrides_body {
+    ($override_body:block, $catch_body:block) => {
+        std::panic::catch_unwind(|| $override_body).ok().unwrap_or_else(|| $catch_body)
+    };
+}
+
